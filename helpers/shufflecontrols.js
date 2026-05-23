b9451e1f4d7aadc412e9eacf516a88549bdc66ea
Jesse <bigjmn@gmail.com>
2026-05-23 13:35:35 -0400

    piecesweight

--- helpers/shufflecontrols.js
+++ helpers/shufflecontrols.js
@@ -1,13 +1,27 @@
+const PIECES_WEIGHT = 1.2
+// module.exports = function newcontrols(socketlist){
+//   var usernum = socketlist.length
+//   socketlist.forEach((item, i) => {
+//     item.rights['right'] = Math.random() < .5/usernum
+//     item.rights['left'] = Math.random() < .5/usernum
+//     item.rights['rotate'] = Math.random() < .5/usernum
+//   });
+//   ['right', 'left', 'rotate'].forEach((item, i) => {
+//     socketlist[Math.floor(usernum*Math.random())].rights[item] = true;
+//   });
+
+
+
+// }
+
 module.exports = function newcontrols(socketlist){
   var usernum = socketlist.length
   socketlist.forEach((item, i) => {
-    item.rights['right'] = Math.random() < .5/usernum
-    item.rights['left'] = Math.random() < .5/usernum
-    item.rights['rotate'] = Math.random() < .5/usernum
+    item.rights['right'] = Math.random() < PIECES_WEIGHT/usernum
+    item.rights['left'] = Math.random() < PIECES_WEIGHT/usernum
+    item.rights['rotate'] = Math.random() < PIECES_WEIGHT/usernum
   });
-  ['right', 'left', 'rotate'].forEach((item, i) => {
-    socketlist[Math.floor(usernum*Math.random())].rights[item] = true;
-  });
+  
 
 
 
