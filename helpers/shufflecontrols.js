7497dc20f45bb0ce40b3dd7141d436708f775814
Jesse <bigjmn@gmail.com>
2026-05-25 17:22:59 -0400

    control options

--- helpers/shufflecontrols.js
+++ helpers/shufflecontrols.js
@@ -1,20 +1,25 @@
-const PIECES_WEIGHT = 1.2
-// module.exports = function newcontrols(socketlist){
-//   var usernum = socketlist.length
-//   socketlist.forEach((item, i) => {
-//     item.rights['right'] = Math.random() < .5/usernum
-//     item.rights['left'] = Math.random() < .5/usernum
-//     item.rights['rotate'] = Math.random() < .5/usernum
-//   });
-//   ['right', 'left', 'rotate'].forEach((item, i) => {
-//     socketlist[Math.floor(usernum*Math.random())].rights[item] = true;
-//   });
+const PIECES_WEIGHT = 1.5 
+const CONTROL_VERSION = "alpha"
+function newcontrolsalpha(socketlist){
+  var usernum = socketlist.length
+  socketlist.forEach((item, i) => {
+    item.rights['right'] = Math.random() < .5/usernum
+    item.rights['left'] = Math.random() < .5/usernum
+    item.rights['rotate'] = Math.random() < .5/usernum
+  });
+  ['right', 'left', 'rotate'].forEach((item, i) => {
+    if (Math.random() < .9){
+      socketlist[Math.floor(usernum*Math.random())].rights[item] = true;
+
+    }
+    
+  });
 
 
 
-// }
+}
 
-module.exports = function newcontrols(socketlist){
+function newcontrolsbeta(socketlist){
   var usernum = socketlist.length
   socketlist.forEach((item, i) => {
     item.rights['right'] = Math.random() < PIECES_WEIGHT/usernum
@@ -25,4 +30,12 @@ module.exports = function newcontrols(socketlist){
 
 
 
+}
+
+module.exports = function newcontrols(socketlist){
+  if (CONTROL_VERSION === "alpha"){
+    newcontrolsalpha(socketlist)
+  } else {
+    newcontrolsbeta(socketlist)
+  }
 }
